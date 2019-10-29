1cf2ff5a4a68729949d956bae85285e5aeb1f285
hanksky <hanksky#yahoo.com.tw>
2019-10-29 22:36:30 +0800

    FREE_FOOD_第二圈完成

--- 專題/FREE TIME.py
+++ 專題/FREE TIME.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import os
+import time
+
 user_agenttt="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
 headers={"user-agent":user_agenttt,
 "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
@@ -26,13 +28,35 @@ soup_first = BeautifulSoup(res_first.text, 'html.parser')
 titles_first = soup_first.select('p')
 for title_first in titles_first:
     try:
-        print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-        #print(title)
-        url_second = "https://food.ltn.com.tw/" + title_first.a["href"]
-        #print(insideurl)
-        res_second = requests.get(url_second, headers=headers)
+        print("~~~~~~~~~~~~~~~~~~2_level~~~~~~~~~~~~~~~~~~~~~")
+        #第二層第1頁
+        url_second_level= "https://food.ltn.com.tw/" + title_first.a["href"] + "/" + str(1)
+        res_second = requests.get(url_second_level, headers=headers)
         soup_second = BeautifulSoup(res_second.text, 'html.parser')
-        soup_second.select()
+        page_tail=soup_second.select('a[class="p_last"]')
+        page_last_number = page_tail[0]['href'].split("/")[5]
+        # 第二層第2頁~最後一頁
+        page_number=1
+        while page_number <= int(page_last_number):
+            url_second_level_every_page = "https://food.ltn.com.tw/" + title_first.a["href"]+"/"+str(page_number)
+            res_second_every_page = requests.get(url_second_level_every_page, headers=headers)
+            soup_second_every_page = BeautifulSoup(res_second_every_page.text, 'html.parser')
+            url_thrds = soup_second_every_page.select('div[data-desc="清單"] a')
+            #print(url_thrds)
+            print(str(page_number)+"page"+"~~~~~~~~~~~~~~~~~~~")
+
+            for url_thrd in url_thrds:
+                #print(url_thrd["href"])
+                url_thir_level="https://food.ltn.com.tw/"+url_thrd["href"]
+                print(url_thir_level)
+            page_number += 1
+
+
+
+
+
+
+
     except TypeError as e:
         print(e)
 
