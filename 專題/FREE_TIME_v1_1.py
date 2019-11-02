702a32b2e7d37f3d72e0baed856295358247b362
hanksky <hanksky@yahoo.com.tw>
2019-11-03 01:45:46 +0800

    第一階段加速器完成

--- 專題/FREE_TIME_v1_1.py
+++ 專題/FREE_TIME_v1_1.py
@@ -19,54 +19,79 @@ path=r'./food'  #資料夾
 if not os.path.exists(path): #沒有這個資料夾就新創資料夾
     os.mkdir(path)
 
-def producer():
+def producer(titles_firsts,queue):
+    for title_first in titles_firsts:
+        try:
+            print("~~~~~~~~~~~~~~~~~~the_second_level~~~~~~~~~~~~~~~~~~~~~")
+            # 第二層第1頁
+            # 跳過兩個分類，裡面頁面沒有內容，原本用except IndexError跳過，但會造成後面爬蟲在一個分類沒爬完，跳到下一個分類
+            if (title_first.a["href"] != "type/84") and (title_first.a["href"] != "type/87"):
+                queue.put(title_first.a["href"])
+        except TypeError as e:
+            print(e)  # 在第一層有些 tag p 下面沒有 tag a
 
+def worker(worker_id,queue):
+    while True:
+        title_first=queue.get()
+        print(title_first)
+        url_second_level = "https://food.ltn.com.tw/" + title_first + "/" + str(1)
+        res_second = requests.get(url_second_level, headers=headers)
+        soup_second = BeautifulSoup(res_second.text, 'html.parser')
+        # 找最後一頁數字
+        page_tail = soup_second.select('a[class="p_last"]')
+        page_last_number = page_tail[0]['href'].split("/")[5]
+        # print(page_last_number)
+        # 第二層第1頁~最後一頁
+        page_number = 1
+        while page_number <= int(page_last_number):
+            url_second_level_every_page = "https://food.ltn.com.tw/" + title_first + "/" + str(
+                page_number)
+            res_second_every_page = requests.get(url_second_level_every_page, headers=headers)
+            soup_second_every_page = BeautifulSoup(res_second_every_page.text, 'html.parser')
+            url_thrds = soup_second_every_page.select('div[data-desc="清單"] a')
+            for url_thrd in url_thrds:
+                url_third_level = "https://food.ltn.com.tw/" + url_thrd["href"]
+                print(url_third_level)
+            print(str(page_number) + "page" + "~~~~~~~~~~~~~~~~~~~")
+            page_number += 1
 
-
-def worker():
-
-
+def first(queue):
+    # 第一層
+    url_first = "https://food.ltn.com.tw/category"
+    res_first = requests.get(url_first, headers=headers)
+    soup_first = BeautifulSoup(res_first.text, 'html.parser')
+    # tag p包含我要的五穀雜糧、肉類..而且五穀雜糧下面的細項不點選就已經全在五穀頁面裡面，到下層連結只要用五穀雜糧就可抓到全部
+    titles_first = soup_first.select('p')
+    producer(titles_first,queue)
 
 def main():
-    queue=mp.JoinableQueue
-        mp.Pool()
-    pool.map
+    t0 = time.time()
+    print("start")
+    queue=mp.JoinableQueue()
+    worker1=mp.Process(target=worker,args=(1,queue))
+    worker1.daemon=True
+    worker1.start()
+    worker2=mp.Process(target=worker,args=(1,queue))
+    worker2.daemon=True
+    worker2.start()
+    worker3 = mp.Process(target=worker, args=(1, queue))
+    worker3.daemon = True
+    worker3.start()
+    worker4 = mp.Process(target=worker, args=(1, queue))
+    worker4.daemon = True
+    worker4.start()
+
+    first(queue)
+    queue.join()
+
+    print(time.time()-t0, "seconds time")
 
 
 
-
-#第一層
-url_first="https://food.ltn.com.tw/category"
-res_first = requests.get(url_first, headers=headers)
-soup_first = BeautifulSoup(res_first.text, 'html.parser')
-#tag p包含我要的五穀雜糧、肉類..而且五穀雜糧下面的細項不點選就已經全在五穀頁面裡面，到下層連結只要用五穀雜糧就可抓到全部
-titles_first = soup_first.select('p')
-for title_first in titles_first:
-    try:
-        print("~~~~~~~~~~~~~~~~~~the_second_level~~~~~~~~~~~~~~~~~~~~~")
-        #第二層第1頁
-        #跳過兩個分類，裡面頁面沒有內容，原本用except IndexError跳過，但會造成後面爬蟲在一個分類沒爬完，跳到下一個分類
-        if (title_first.a["href"] != "type/84") and (title_first.a["href"] != "type/87"):
-            url_second_level= "https://food.ltn.com.tw/" + title_first.a["href"] + "/" + str(1)
-            res_second = requests.get(url_second_level, headers=headers)
-
-            soup_second = BeautifulSoup(res_second.text, 'html.parser')
-            #找最後一頁數字
-            page_tail=soup_second.select('a[class="p_last"]')
-            page_last_number = page_tail[0]['href'].split("/")[5]
-            #print(page_last_number)
-            # 第二層第1頁~最後一頁
-            page_number=1
-            while page_number <= int(page_last_number):
-                url_second_level_every_page = "https://food.ltn.com.tw/" + title_first.a["href"]+"/"+str(page_number)
-                res_second_every_page = requests.get(url_second_level_every_page, headers=headers)
-                soup_second_every_page = BeautifulSoup(res_second_every_page.text, 'html.parser')
-                url_thrds = soup_second_every_page.select('div[data-desc="清單"] a')
-                print(str(page_number) + "page" + "~~~~~~~~~~~~~~~~~~~")
-                page_number += 1
-    except TypeError as e:
-        print(e)#在第一層有些 tag p 下面沒有 tag a
+if __name__ == "__main__":
+    main()
 
 
-with open('FOOD.txt', 'w', encoding='utf-8') as f:
-    f.write(json)
+
+# with open('FOOD.txt', 'w', encoding='utf-8') as f:
+#     f.write(json)
