92af7c002d9f86095979acef9c68b98adadf09db
hanksky <hanksky@yahoo.com.tw>
2019-11-24 00:15:10 +0800

    清理FREE_FOOD 無法解決空白問題

--- 專題/ETL.py
+++ 專題/ETL.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[25]:
 
 
 import pandas as pd
 import os,json,re
-path=r"F:\資策會\專題\爬蟲\venv\res"
+path=r"F:\資策會\專題\爬蟲\venv\collctions"
 #讀所有json
 json_list=os.listdir(path)
 json_list
 
 
-# In[2]:
+# In[26]:
 
 
-df=pd.DataFrame(columns=["檔名","食譜名稱","食材與單位","步驟"])
+df=pd.DataFrame(columns=["食譜名稱","食材與單位","步驟"])
 df
 
 
-# In[12]:
+# In[27]:
 
 
 for i in json_list:
@@ -29,15 +29,16 @@ for i in json_list:
         
                 dic=f.read()
                 d=json.loads(dic)
-                food_list=[]
-                food_list.append(d["recipe_name"])
-                food_list.append(d["quantity"])
-                #print(food_list)
-                df=df.append(pd.DataFrame(food_list,columns=["檔名","份數"]))
+                food_list=[[]]
+                food_list[0].append(d["recipe_name"])
+                food_list[0].append(d["ingredients"])
+                food_list[0].append(d["cooking_steps"])
+                print(food_list)
+                df=df.append(pd.DataFrame(food_list,columns=["食譜名稱","食材與單位","步驟"]))
             except ValueError as e:
                 print(e)
-            except JSONDecodeError as e:
-                print(e)
+            #except JSONDecodeError as e:
+                #print(e)
     except PermissionError as e:
         print(e)
     except FileNotFoundError as e:
@@ -57,7 +58,7 @@ for i in json_list:
 
 def column_filter(s):
     #x= re.split('【|】|▪|：|•',s)[-1]
-    x=s.replace("］","")
+    x=s.replace("✿","")
     
     return x
 
@@ -71,19 +72,15 @@ except IndexError as e:
     print(e)
 except KeyError as e:
     print(e)
+except AttributeError as e:
+    print(e)
 df
 
 
-# In[11]:
+# In[24]:
 
 
-df.to_csv(r'./bbb.csv',index=0,encoding="utf-8")
-
-
-# In[ ]:
-
-
-
+df.to_csv(r'./icook.csv',index=0,encoding="utf-8")
 
 
 # In[ ]:
@@ -92,10 +89,62 @@ df.to_csv(r'./bbb.csv',index=0,encoding="utf-8")
 
 
 
-# In[ ]:
+# In[28]:
+
+
+ddf=pd.DataFrame(columns=["食譜名稱","食材與單位"])
+ddf
+
+
+# In[29]:
 
 
 
+foodd_list=[["陳皮紅豆湯圓x","[{'ingredient_names': '雞蛋蛋黃3顆', 'ingredient_units': '雞蛋蛋黃3顆'}]"],["花生x","[{'ingredient_names': '芝麻油10g', 'ingredient_units': '芝麻油10g'}]"]]
+ddf=ddf.append(pd.DataFrame(foodd_list,columns=["食譜名稱","食材與單位"]))
+ddf
+
+
+# In[30]:
+
+
+def column_filter(s):
+    #x= re.split('【|】|▪|：|•',s)[-1]
+    x=s.replace("✿","")
+    
+    return x
+
+
+# In[82]:
+
+
+for i in json_list:
+    try:
+        with open(path+"/"+i, "r", encoding="utf-8") as f:
+            try:
+        
+                dic=f.read()
+                d=json.loads(dic)
+                #print(d)
+                list_ingredients=d["ingredients"]
+                #print(list_ingredients)
+                for i in list_ingredients:
+                    ingredient_names=i["ingredient_names"]
+                    #print(ingredient_names)
+                    a=re.split(r"\d",ingredient_names)[0].strip().split("（")[0].replace("少許","").replace("適量","").replace("少量","").replace("半","0.5")
+                    b=re.split(r"\d",a)[0].replace("依個人喜好添加","").replace("適當","").strip().split("、")[0].split("﹙")[0].split("(")[0]                    .replace("新鮮","").replace("手工","").split("或")[0].replace("基底","").split("約")[0]
+                    print(b)
+
+                    
+                
+            except ValueError as e:
+                print(e)
+            except JSONDecodeError as e:
+                print(e)
+    except PermissionError as e:
+        print(e)
+    except FileNotFoundError as e:
+        print(e)
 
 
 # In[ ]:
