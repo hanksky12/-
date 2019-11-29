9dd8c855eedcedb9a593a169e1b929dccc074790
hanksky <hanksky#yahoo.com.tw>
2019-11-29 17:41:19 +0800

    網站正規抓的到，python抓不到

--- 專題/ETL.py
+++ 專題/ETL.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os,json,re,csv
 
-path=r"F:\資策會\專題\爬蟲\venv\collction_freefood_11_23"
+path=r"E:\BIG DATA下載\專題\爬蟲\venv\collction_freefood_11_23"
 
 
 
@@ -14,13 +14,35 @@ path=r"F:\資策會\專題\爬蟲\venv\collction_freefood_11_23"
 #             pass
 #
 # trans("１","FF10","uFF19")
+def split_ingredient_units_to_number(ingredient_units_clean):
+    try:
+        x=re.search(r"\d*.?\d",ingredient_units_clean)
+        if x:
+            number=x.group()
+            return number+"xxxxx"
+    except TypeError as e:
+        print(e)
 
 
 
-def csv_to_out(ingredient_names_clean):
-    with open('free_food_same_name.csv', 'a+', newline='', encoding="utf-8") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow([ingredient_names_clean])
+def split_ingredient_units_to_unit(ingredient_units_clean):
+    try:
+        x=re.search(r"[^0-9.]",ingredient_units_clean)
+        if x:
+            #print(x.group())
+            unit=x.group()
+            return unit
+    except TypeError as e:
+        print(e)
+def csv_to_out(ingredient_names_clean,number,unit):
+    with open('free_food_clean.csv', 'a+', newline='', encoding="utf-8") as csvfile:
+        try:
+            writer = csv.writer(csvfile)
+            writer.writerow([ingredient_names_clean,number,unit])
+        except NameError as e:
+            print(e)
+        except JSONDecodeError as e:
+            print(e)
 
 def fraction_to_float(s):
     list_str=list(s.group())
@@ -45,7 +67,7 @@ def clean_unit(ingredient_units):
     # 要有順序性的取代，否則有反效果，一開始就用RE模組可能誤抓
     ingredient_units = re.sub(r"半\b", "", ingredient_units).replace("1小半", "0.5").replace("小半", "0.5") \
         .replace("一半", "0.5").replace("半", "0.5").replace("1½", "1.5").replace("½", "0.5").replace("⅓", "0.333") \
-        .replace("數", "x") \
+        .replace("數", "x").replace("／","/").replace("～","~")\
         .replace("一般", "").replace("一", "1").replace("１", "1").replace("０", "1").replace("２", "2") \
         .replace("３", "3").replace("４", "4").replace("５", "5").replace("（", "(").replace("）", ")")
     # 找全形數字
@@ -57,7 +79,7 @@ def clean_unit(ingredient_units):
     # 怕這些形容詞在真正的單位詞前面
     list_ingredient_units = ["適量", "酌量", "少量", "幾片", "幾滴", "少許", "些許"]
     for n, j in enumerate(list_ingredient_units):
-        ingredient_units = ingredient_units.replace(str(j), str(n + 10000))
+        ingredient_units = ingredient_units.replace(str(j),"5g")
     try:
         ingredient_units = re.search(r"(([0-9]|x).*)", ingredient_units)
         ingredient_units = ingredient_units.group()
@@ -66,6 +88,8 @@ def clean_unit(ingredient_units):
         ingredient_units = re.sub(r"c+\.c+\.?", "cc", ingredient_units)
         ingredient_units = ingredient_units.replace("，依個人喜好調整", "").replace("約", "").replace(",", "")
         ingredient_units = re.sub(r"\s", "", ingredient_units)
+
+        ingredient_units = re.sub(r"的量*", "", ingredient_units)
         #轉換數量從分數變成小數 1/2=>0.5
         ingredient_units = re.sub(r"\d(\／|\/)\d.*", fraction_to_float, ingredient_units)
         x = re.search(r"\d*(k)?(g|公克|克)", ingredient_units)
@@ -73,11 +97,14 @@ def clean_unit(ingredient_units):
             # 有KG被抓進來
             ingredient_units = x.group()
             return ingredient_units
-            # print(ingredient_units, "               ", ingredient_names, "          ", e)
         else:
-            pass
-            return ingredient_units
-            #print(ingredient_units, "               ", ingredient_names, "          ", e)
+            y= re.search(r"(\~|\∼).*", ingredient_units)
+            if y:
+                ingredient_units = y.group()
+                ingredient_units=ingredient_units.replace("~","").replace("∼","")
+                return ingredient_units
+            else:
+                return ingredient_units
     except NameError as e:
         print(e)
     except AttributeError as e:
@@ -100,13 +127,17 @@ def main():
                         ingredient_names = i["ingredient_names"]
                         ingredient_names_clean=clean_food(ingredient_names)
 
-                        # 輸出給裕璋 建立食材同義詞庫
-                        #csv_to_out(ingredient_names_clean)
-
-                        #清理單位
+                        #清理數量與單位
                         ingredient_units=i["ingredient_units"]
                         ingredient_units_clean=clean_unit(ingredient_units)
-                        print(ingredient_units_clean,"        ",ingredient_names,"       ",ingredient_units_clean)
+
+                        #分隔數量與單位
+                        number=split_ingredient_units_to_number(ingredient_units_clean)
+                        unit=split_ingredient_units_to_unit(ingredient_units_clean)
+                        #輸出CSV
+                        #csv_to_out(ingredient_names_clean,number,unit)
+
+                        print(ingredient_units_clean,"        ","   ",unit)
                 except ValueError as e:
                     print(e)
                 except JSONDecodeError as e:
